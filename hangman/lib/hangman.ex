f8cedb954b3845f273c473e8a3ff973fab5cd295
Nick Rokosz <nicholasrokosz@gmail.com>
2024-03-07 22:04:42 -0600

    Add game module types and new_game function

--- hangman/lib/hangman.ex
+++ hangman/lib/hangman.ex
@@ -1,7 +1,19 @@
 defmodule Hangman do
-  def new_game do
-  end
+  alias Hangman.Impl.Game
 
-  def make_move(game, guess) do
+  @opaque game :: Game.t()
+  @type state :: :initializing | :won | :lost | :good_guess | :bad_guess | :already_used
+  @type tally :: %{
+          turns_left: integer(),
+          game_state: state,
+          letters: list(String.t()),
+          used: list(String.t())
+        }
+
+  @spec new_game() :: game
+  defdelegate new_game, to: Game
+
+  @spec make_move(game, String.t()) :: {game, tally}
+  def make_move(_game, _guess) do
   end
 end
