58c57e71fddb5906e5513b56f86b5d968de8f7a1
Nick Rokosz <nicholasrokosz@gmail.com>
2024-03-08 14:40:19 -0600

    Add game module

---
+++ hangman/lib/impl/game.ex
@@ -0,0 +1,27 @@
+defmodule Hangman.Impl.Game do
+  @type t :: %Hangman.Impl.Game{
+          turns_left: integer,
+          game_state: Hangman.state(),
+          letters: list(String.t()),
+          used: MapSet.t(String.t())
+        }
+
+  defstruct(
+    turns_left: 7,
+    game_state: :initializing,
+    letters: [],
+    used: MapSet.new()
+  )
+
+  @spec new_game :: t
+  def new_game do
+    new_game(Dictionary.random_word())
+  end
+
+  @spec new_game(String.t()) :: t
+  def new_game(word) do
+    %Hangman.Impl.Game{
+      letters: word |> String.codepoints()
+    }
+  end
+end
