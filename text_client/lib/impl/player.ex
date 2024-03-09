3d5bfd6cddee832741bf20670df2cb5a33ac0321
Nick Rokosz <nicholasrokosz@gmail.com>
2024-03-09 15:12:33 -0600

    Output game state

--- text_client/lib/impl/player.ex
+++ text_client/lib/impl/player.ex
@@ -23,7 +23,7 @@ defmodule TextClient.Impl.Player do
 
   def interact({_game, tally}) do
     IO.puts(feedback_for_tally(tally))
-    # display state
+    IO.puts(current_word(tally))
     # get next guess
     # make move
     # recurse
@@ -33,10 +33,23 @@ defmodule TextClient.Impl.Player do
 
   # :initializing | :good_guess | :bad_guess | :already_used
   def feedback_for_tally(tally = %{game_state: :initializing}) do
-    "Welcome! I'm thinking of a #{tally.letters |> length} letter word."
+    "Welcome! The target word is #{tally.letters |> length} letters long."
   end
 
   def feedback_for_tally(%{game_state: :good_guess}), do: "Good guess!"
   def feedback_for_tally(%{game_state: :bad_guess}), do: "Sorry, that letter isn't in the word."
   def feedback_for_tally(%{game_state: :already_used}), do: "You already guessed that letter."
+
+  ##############################################################################################################
+
+  def current_word(tally) do
+    """
+    Guesses remaining: #{tally.turns_left |> to_string()}
+    #{letters_guessed_output(tally.used)}
+    Word: #{tally.letters |> Enum.join(" ")}
+    """
+  end
+
+  def letters_guessed_output(_used = []), do: ""
+  def letters_guessed_output(used), do: "Letters guessed: #{used |> Enum.join(", ")}"
 end
